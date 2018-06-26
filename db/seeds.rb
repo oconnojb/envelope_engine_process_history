fdcdbfd6474660b9111b79df15e40140c3b27cff
John O'Connor <oconnojb@gmail.com>
2018-06-26 17:54:03 +0000

    seed.rb 2/2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,86 @@
+def make_inprod
+  nbr = Random.rand(1..3)
+  aule = ''
+  case nbr
+  when 1
+    6.times do
+      aule += Random.rand(10).to_s
+    end
+  when 2
+    aule += 'QP00'
+    3.times do
+      aule += Random.rand(10).to_s
+    end
+  when 3
+    2.times do
+      aule += Random.rand(10).to_s
+    end
+    aule += '-'
+    4.times do
+      aule += Random.rand(10).to_s
+    end
+  end
+  return aule
+end
+
+def make_model
+  models = ['ENVBO', 'ENVSP', 'ENVPR']
+  return models[Random.rand(0..2)]
+end
+
+def make_item
+  first = ['BWE', 'SWE']
+  second = ['A1', 'A2', 'A6']
+  return first[Random.rand(2)] + '-' + second[Random.rand(3)]
+end
+
+def make_fillcount
+  return Random.rand(875...10000)
+end
+
+def make_inshtreq
+  nmr = Random.rand(3)
+  case nmr
+  when 0
+    return 0
+  when 1
+    return Random.rand(10000...100000)
+  when 2
+    return Random.rand(100000...1000000)
+  end
+end
+
+def make_netreqd
+  nmr = Random.rand(2)
+  case nmr
+  when 0
+    return '0'
+  when 1
+    return Random.rand(10..20).to_s + '.000'
+  end
+end
+
+def make_available
+  aule = Random.rand(100...10000).to_s
+  return aule += '.000'
+end
+
+def make_availshts
+  nmr = Random.rand(3)
+  case nmr
+  when 0
+    return 0
+  when 1
+    return Random.rand(1..50)
+  when 2
+    return Random.rand(90000...100000)
+  end
+end
+
+def make_thisid
+  return Random.rand(10000...100000)
+end
+
 def whse
   nbr = Random.rand(1..3)
   case nbr
